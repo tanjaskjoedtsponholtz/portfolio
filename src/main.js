e2b1fb2d13d82c5c40497c93fd53d94f73ad9b1a
tanjasponholtz <tanj8868@stud.kea.dk>
2022-09-26 10:46:41 +0200

    comit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+animate(
+  " h1",
+  { opacity: [0, 1] },
+  {
+    delay: stagger(0.5, { start: 1 }),
+  }
+);
